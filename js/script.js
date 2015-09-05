9607e342e755c12096dac43ed819992a36eb7eae
limoozyn <limoozyn@gmail.com>
2015-09-06 00:51:32 +0300

    Move Partner section back below the schedule and little changes to sticky menu

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ $(document).ready(function() {
 
   $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $( $.attr(this, 'href') ).offset().top-50
     }, 500);
     return false;
   });
